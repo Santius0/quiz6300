88d7681c8472be1ad121288344ee2d2f976ca799
santius0 <santius1995@gmail.com>
2022-04-17 11:45:16 -0400

    bunch of functional changes

--- src/components/Question.component.jsx
+++ src/components/Question.component.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import "./Question.css";
 
 const QuestionComponent = props => {
+
+    const [selected, setSelected] = useState(null);
+    const [answers, setAnswers] = useState([]);
+
+    useEffect(() => {
+        if(props.question.type === 'multiple') {
+            setAnswers(props.question.incorrect_answers.concat(props.question.correct_answer)
+                .map(value => ({value, sort: Math.random()}))
+                .sort((a, b) => a.sort - b.sort)
+                .map(({value}) => value));
+        } else {
+            setAnswers(["True", "False"]);
+        }
+    }, [])
+
+
+    const handleSelect = (answerIndex) => {
+        if(answerIndex !== selected){
+            setSelected(answerIndex);
+        }
+    }
+
+    const submitAnswer = () => {
+        props.onAnswer(answers[selected] === props.question.correct_answer);
+    }
+
     return(
-        <p>{props.key + ". " + props.question.toString()}</p>
+        <div>
+            <h1>{props.question.question}</h1>
+            {answers.map((value, index) => (
+                <p className={selected === index ? "selected" : ""} key={index} onClick={() => handleSelect(index)}>{value}</p>
+            ))}
+            <button disabled={selected === null} onClick={submitAnswer}>Answer</button>
+        </div>
     );
 }
 
--- src/components/Quiz.component.jsx
+++ src/components/Quiz.component.jsx
@@ -1,24 +1,51 @@
 import React, {useEffect, useState} from "react";
 import QuestionComponent from "./Question.component";
 
-import "./form.css";
+// import "./form.css";
 
 const QuizComponent = () => {
 
-    const [quizConfig, setQuizConfig] = useState({
+    const defaultQuizConfig = {
         playerName: "",
-        numQuestions: 10,
+        numQuestions: 1,
         category: null,
         difficulty: null,
         questionType: null,
         sessionToken: null,
-    });
+    };
+
+    const defaultQuizState = {
+        loading: false,
+        currQuestion: 0,
+        currScore: 0,
+        started: false,
+        finished: false,
+    };
+
+    const [quizConfig, setQuizConfig] = useState(defaultQuizConfig);
+
+    const [quizState, setQuizState] = useState(defaultQuizState);
+
     const [questions, setQuestions] = useState([]);
 
     useEffect(() => {
         fetchSessionToken();
-        // fetchQuestions();
+        fetchQuestions();
     }, [])
+
+    const updateQuizConfigItem = (key, value) => {
+        setQuizConfig(prevState => ({
+            ...prevState,
+            [key]: value
+        }))
+    }
+
+    const updateQuizStateItem = (key, value) => {
+        setQuizState(prevState => ({
+            ...prevState,
+            [key]: value
+        }))
+    }
 
     const generateQuestionRequestUrl = () => {
         let url = "https://opentdb.com/api.php?";
@@ -31,13 +58,14 @@ const QuizComponent = () => {
     }
 
     const fetchQuestions = () => {
+        updateQuizStateItem("loading", true);
         fetch(generateQuestionRequestUrl())
             .then(res =>{
                 return res.json();
             })
             .then(body => {
-                console.log(body.results);
                 setQuestions(body.results);
+                updateQuizStateItem("loading", false);
             })
             .catch(err => {
                 console.log(err)
@@ -45,6 +73,7 @@ const QuizComponent = () => {
     }
 
     const fetchSessionToken = () => {
+        updateQuizStateItem("loading", true);
         const url = "https://opentdb.com/api_token.php?command=request";
         if(quizConfig.sessionToken) resetSessionToken();
         else {
@@ -53,10 +82,8 @@ const QuizComponent = () => {
                     return res.json();
                 })
                 .then(body => {
-                    setQuizConfig(prevState => ({
-                        ...prevState,
-                        sessionToken: body.token
-                    }));
+                    updateQuizConfigItem("sessionToken", body.token);
+                    updateQuizStateItem("loading", false);
                 })
                 .catch(err => {
                     console.log(err);
@@ -65,28 +92,81 @@ const QuizComponent = () => {
     }
 
     const resetSessionToken = () => {
-      const url = "https://opentdb.com/api_token.php?command=reset&token=" + quizConfig.sessionToken;
-      fetch(url)
-          .then(res => {
-              return res.json();
-          })
-          .then(body => {
-              setQuizConfig(prevState => ({
-                  ...prevState,
-                  sessionToken: body.token
-              }));
-          })
-          .catch(err  => {
-              console.log(err);
-          });
+        updateQuizStateItem("loading", true);
+        const url = "https://opentdb.com/api_token.php?command=reset&token=" + quizConfig.sessionToken;
+        fetch(url)
+            .then(res => {
+                return res.json();
+            })
+            .then(body => {
+                updateQuizConfigItem("sessionToken", body.token);
+                updateQuizStateItem("loading", false);
+            })
+            .catch(err  => {
+                console.log(err);
+            });
+    }
+
+    const handleConfigChange = e => {
+        updateQuizConfigItem(e.target.name, e.target.value);
+    }
+
+    const handleOnAnswer = (correct) => {
+        if(correct) updateQuizStateItem("currScore", quizState.currScore + 1);
+        nextQuestion();
+    }
+
+    const nextQuestion = () => {
+        if(quizState.currQuestion < questions.length - 1) updateQuizStateItem("currQuestion", quizState.currQuestion + 1)
+        else finishQuiz();
+    }
+
+    const reset = () => {
+        // setQuizConfig(defaultQuizConfig);
+        setQuizState(defaultQuizState);
+        setQuestions([]);
+        fetchQuestions();
+    }
+
+    const startQuiz = () => {
+        updateQuizStateItem("started", true);
+    }
+
+    const finishQuiz = () => {
+      updateQuizStateItem("finished", true);
+    }
+
+    let questionComponents = [];
+    for(let i=0; i<questions.length; i++){
+        questionComponents.push(<QuestionComponent key={i} question={questions[i]} onAnswer={handleOnAnswer}/>);
+    }
+
+    if(quizState.loading) return <h1>Loading...</h1>
+
+    if(!quizState.started){
+        return (
+            <form>
+                <input type="text" value={quizConfig.playerName} onChange={handleConfigChange} name="playerName" placeholder="name"/>
+                <button type="submit" onClick={e => {e.preventDefault(); startQuiz();}}>Start Quiz</button>
+            </form>
+        );
+    }
+
+    if(quizState.finished){
+        return (
+            <div>
+                <h3>Final Score: {quizState.currScore}/{questions.length}</h3>
+                <button onClick={e => {e.preventDefault(); reset();}}>Restart</button>
+            </div>
+        );
     }
 
     return(
         <div>
             <div className="react-form-container"> </div>
-            {questions.map((value, index) => (
-                <QuestionComponent key={index} question={value}/>
-            ))}
+            <h3>Question: {quizState.currQuestion + 1}/{questions.length}</h3>
+            <h3>Score: {quizState.currScore}</h3>
+            {questionComponents[quizState.currQuestion]}
         </div>
     );
 }
